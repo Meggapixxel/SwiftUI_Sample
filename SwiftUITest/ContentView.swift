68f2d80c32972fc8391681c4e972dbefb536e0fc
Meggapixxel <zhydenkodeveloper@gmail.com>
2019-11-12 19:13:55 +0200

    Initial Commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Vadim Zhydenko on 12.11.2019.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
